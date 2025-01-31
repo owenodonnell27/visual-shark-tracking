ce55a9365694611b35d907d2bcbaec9c1f42c80d
owenodonnell27 <odonnellopo@gmail.com>
2025-01-31 14:34:53 -0500

    update image names

--- rename.py
+++ rename.py
@@ -2,29 +2,21 @@ import os
 
 # Use this file/function to rename data
 def rename_files_in_directory(root_dir):
-    S = set()
+    S = set() 
     i = 0
     for subdir, dirs, files in os.walk(root_dir):
         for i, file in enumerate(files):
-            if file.endswith(('.jpg', '.png', '.jpeg', '.JPG', '.gif')):  
+            if not file.endswith(('.txt', 'json')):  
                 
-                S.add(subdir.split('\\')[2])
-                print(i)
-                i += 1
+                new_file_name = file + '.jpg'
 
+                # Full file paths
+                old_file_path = os.path.join(subdir, file)
+                new_file_path = os.path.join(subdir, new_file_name)
                 
-                # # Generate new file name based on directory name and index
-                # directory_name = os.path.basename(subdir)
-                # new_file_name = f"{directory_name}_{i+1:03d}{os.path.splitext(file)[1]}"
-                
-                # # Full file paths
-                # old_file_path = os.path.join(subdir, file)
-                # new_file_path = os.path.join(subdir, new_file_name)
-                
-                # # Rename the file
-                # os.rename(old_file_path, new_file_path)
-                # print(f'Renamed: {old_file_path} -> {new_file_path}')
-    print(S)
+                # Rename the file
+                os.rename(old_file_path, new_file_path)
+                print(f'Renamed: {old_file_path} -> {new_file_path}')
 
 # Define the root directory you want to process
 root_directory = '.\\images'
