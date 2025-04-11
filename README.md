4c20e97f359e454d315a7fa255efce4a09f5e5f7
owenodonnell27 <odonnellopo@gmail.com>
2025-04-11 13:40:45 -0400

    Clean up

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Visual Shark Tracking
+
 UConn and OWL Integration's Senior Design Project
+
+# Model Training
+
+Due to the large size of the .m5 and .tflite files, GitHub will not allow them to be pushed to a remote repository. So in order to get the .tflite file to upload to a coral board, you must first train the model using one of the Jupyter Notebooks and then use converter.py to convert the .h5 file you get from model training to a .tflite file.
+
+Images used in training are stored in the images zip file. Depending on what model you are training you may need to restructure the organization of the images. If using shark.ipynb remove images from the shark and no shark folders and move them into their parent folder. For example, for the training images, move all images in the shark and no shark folders into the train folders. shark_detection_with_transfer_learning.ipynb uses the image organization as is.
+
+Once you have your model trained, in converter.py in the tools folder, replace MODEL FILE NAME HERE with your models name to get a .tflite model and move the .tflite file into the models folder in the board_code folder.
+
+# Coral Board
+
+While in the coral board's shell, create a directory of your choosing. Before being able to upload files to a coral board using scp, you must upload an ssh key to the board. Once you have the key added, use scp to copy all models in the models folder, coral_fswebcam.py, coral_uart.py, and main.py to your newly created directory.
+
+Make sure you have the camera plugged in and then run main.py.
--- board_code/main.py
+++ board_code/main.py
@@ -1,31 +1,39 @@
-import sys
-import os
-import time
-from queue import Queue
-import tensorflow as tf
 import numpy as np
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
+import tflite_runtime.interpreter as tflite
+from PIL import Image
+import sys
+import time
+import os
+import coral_fswebcam
+import coral_uart
 
-def load_model(model_path, weights_path):
-    model = tf.keras.models.load_model(model_path)
-    model.load_weights(weights_path) 
-    return model
+
+def load_tflite_model(model_path):
+    interpreter = tflite.Interpreter(model_path=model_path)
+    interpreter.allocate_tensors()
+    return interpreter
+
 
 def preprocess_image(img_path, target_size=(256, 256)):
-    img = load_img(img_path, target_size=target_size) 
-    img_array = img_to_array(img) 
-    img_array = np.expand_dims(img_array, axis=0) 
-    img_array /= 255.0 
+    img = Image.open(img_path).convert("RGB")  # Open image and ensure RGB format
+    img = img.resize(target_size)  # Resize to target dimensions
+    img_array = np.array(img).astype(np.float32) / 255.0  # Convert to float32 and normalize
+    img_array = np.expand_dims(img_array, axis=0)  # Expand dimensions for model input
     return img_array
 
 
-def predict(model, img_array, class_names=('non-shark', 'shark')):
-    prediction = model.predict(img_array) 
-
-    predicted_class_index = np.argmax(prediction, axis=1)[0] 
-    predicted_class = class_names[predicted_class_index]  
-
-    confidence = prediction[0][predicted_class_index] 
+def predict(interpreter, img_array, class_names=("non-shark", "shark")):
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    
+    interpreter.set_tensor(input_details[0]['index'], img_array)
+    interpreter.invoke()
+    
+    prediction = interpreter.get_tensor(output_details[0]['index'])
+    predicted_class_index = np.argmax(prediction, axis=1)[0]
+    predicted_class = class_names[predicted_class_index]
+    confidence = prediction[0][predicted_class_index]
+    
     return predicted_class, confidence
 
 
@@ -52,40 +60,31 @@ def print_table(data):
 
 if __name__ == '__main__':
 
-    model_path = weights_path = image_path = './' 
+    # Calling this function with: python3 main.py <model name (no file extension)>
 
     model_data = [
-        {'name': 'initial_model', 'accuracy': 80, 'f1': 'f1 score', 'battery': 'battery usage'},
-        {'name': 'dummy_model', 'accuracy': 100, 'f1': 'Perfect', 'battery': 'None'}
+        {'name': 'initial_model', 'accuracy': 0.80, 'f1': 0.89, 'battery': 'battery usage'},
+        {'name': 'dummy_model', 'accuracy': 100, 'f1': '1.0', 'battery': 'None'}
     ]
 
     if '--table' in sys.argv:
         print_table(model_data)
         sys.exit()
 
-    model_path += sys.argv[1] + '.h5'
-    weights_path += sys.argv[1] + '_weights.h5'
-    model = load_model(model_path, weights_path)
+    model_path = weights_path = './models/' + sys.argv[1]
 
-    image_queue = Queue()
+    model_path += '.tflite'
+    model = load_tflite_model(model_path)
 
     while True:
-        image_names = os.listdir('./new_images')
-        for image in image_names:
-            image_queue.put(image)
 
-            old_path = './new_images/' + image
-            new_path = './queued_images/' + image
-            os.rename(old_path, new_path)
+        time.sleep(5)
+        coral_fswebcam.capture()
 
-        time.sleep(1)
+        img_array = preprocess_image('camera_image.jpg')
+        predicted_class, confidence = predict(model, img_array)
+        print(f'Predicted class: {predicted_class} with confidence: {confidence:.2f}')
 
-        if not image_queue.empty():
-            current_image = image_queue.get()
-            img_array = preprocess_image('./queued_images/' + current_image)
-            predicted_class, confidence = predict(model, img_array)
-            print(f'Predicted class: {predicted_class} with confidence: {confidence:.2f}')
-
-            old_path = './queued_images/' + current_image
-            new_path = './old_images/' + current_image
-            os.rename(old_path, new_path)
+        # TODO: Add code for sending data to packet
+        
+        
